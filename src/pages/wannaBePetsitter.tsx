43dc31b8425af3e09d2a92938be059ff8aef5166
angelicawillrich <angelica@willri.ch>
2023-02-15 15:46:52 +0100

    created wanna be petsitter page

---
+++ src/pages/wannaBePetsitter.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '../components/button'
+
+const WannaBePetSitter = () => (
+  <div className="flex flex-col w-full max-w-xs gap-4 justify-center items-center">
+    <Button primary={false}>Quero ser PetSitter</Button>
+    <Button>Continuar</Button>
+  </div>
+)
+
+export default WannaBePetSitter
