32657f46e9b2d9942ab2f8eed4d0c9c75da9c017
angelicawillrich <angelica@willri.ch>
2023-02-15 15:40:18 +0100

    created register pets page

---
+++ src/pages/registerPets.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import Button from '../components/button'
+import Dropdown from '../components/dropdown'
+import Input from '../components/input'
+import TextArea from '../components/textArea'
+
+const especies = [{ id: 0, value: 'cachorro' }, { id: 1, value: 'gato' }, { id: 3, value: 'pássaro' }]
+
+interface CityState {
+  id: number
+  nome: string
+}
+
+interface State {
+  id: number
+  nome: string
+  municipio: CityState
+}
+
+interface List {
+  id: any
+  value: any
+}
+
+const RegisterPets = () => (
+  <div className="flex flex-col gap-4 justify-center items-center">
+    <h1>Cadastre seu(s) pet(s)</h1>
+    <Input
+      label="Nome*"
+    />
+    <div className="flex gap-2">
+      <Input
+        label="Ano de nascimento*"
+      />
+      <Input
+        label="Peso*"
+      />
+    </div>
+    <div className="flex w-full gap-2">
+      <Dropdown
+        id="especie"
+        label="Espécie*"
+        list={especies}
+        onChange={() => {}}
+      />
+      <Input
+        label="Raca*"
+      />
+    </div>
+    <Input
+      label="Foto"
+      type="file"
+      accept="image/*"
+    />
+    <TextArea
+      label="Outras informacoes"
+      rows={4}
+    />
+    <Button primary={false}>Adicionar + 1 pet</Button>
+    <Button>Continuar</Button>
+    <a href="#">Continuar sem registrar pets</a>
+  </div>
+)
+
+export default RegisterPets
