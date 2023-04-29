c07dcdbae16f4fd6a6516af265c15b32694daaee
angelicawillrich <angelica@willri.ch>
2023-04-29 18:13:23 +0200

    refactored login screen; added a call to the api to logging in

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,39 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 import Logo from '../assets/logo.svg'
 import Button from '../components/baseComponents/button'
 import Input from '../components/baseComponents/input'
 
+interface IForm {
+  email: string
+  password: string
+}
+
+const initialState = {
+  email: '',
+  password: '',
+}
+
 const Login = () => {
   const navigate = useNavigate()
+  const [formData, setFormData] = useState<IForm>(initialState)
+
+  const handleChange = (field: keyof IForm, value: string) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [field]: value,
+    }))
+  }
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+    try {
+      const response = await axios.post(
+        'http://127.0.0.1:3000/login',
+        formData,
+      )
+
+      if (!response.data.user[0].name) {
+        navigate('/personal')
+      } else {
+        navigate('/home')
+      }
+    } catch (error:any) {
+      console.error({ error })
+      alert(JSON.parse(error.request.responseText).message)
+    }
+  }
+
   return (
-    <div className="flex flex-col gap-4 justify-center items-center">
-      <img src={Logo} alt="PetSitter logomarca" />
-      <Input
-        label="E-mail"
-        placeholder="johndoe@example.com"
-      />
-      <Input
-        label="Senha"
-        placeholder="**********"
-        type="password"
-      />
-      <Button>Entrar</Button>
-      <button
-        type="button"
-        className="w-fit text-base decoration-transparent border-b-[1px] p-0 m-0 leading-none"
-        onClick={() => {}}
-      >
-        Esqueceu sua senha?
-      </button>
-      <button
-        type="button"
-        className="w-fit text-base decoration-transparent border-b-[1px] p-0 m-0 leading-none"
-        onClick={() => { navigate('/register') }}
-      >
-        Nao possui conta? Crie uma agora.
-      </button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className="flex flex-col gap-4 justify-center items-center">
+        <img src={Logo} alt="PetSitter logomarca" />
+        <Input
+          name="email"
+          label="E-mail"
+          placeholder="johndoe@example.com"
+          value={formData.email}
+          onChange={(e) => handleChange('email', e.target.value)}
+        />
+        <Input
+          name="password"
+          label="Senha"
+          placeholder="**********"
+          type="password"
+          value={formData.password}
+          onChange={(e) => handleChange('password', e.target.value)}
+        />
+        <Button type="submit">Entrar</Button>
+        <button
+          type="button"
+          className="w-fit text-base decoration-transparent border-b-[1px] p-0 m-0 leading-none"
+          onClick={() => {}}
+        >
+          Esqueceu sua senha?
+        </button>
+        <button
+          type="submit"
+          className="w-fit text-base decoration-transparent border-b-[1px] p-0 m-0 leading-none"
+          onClick={() => navigate('/register')}
+        >
+          Não possui conta? Crie uma agora!
+        </button>
+      </div>
+    </form>
   )
 }
 
