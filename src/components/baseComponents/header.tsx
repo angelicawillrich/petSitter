7f256417dea548eb4f264b1193a52fd40e6043d0
angelicawillrich <angelica@willri.ch>
2023-05-07 20:53:37 +0200

    set new page width

--- src/components/baseComponents/header.tsx
+++ src/components/baseComponents/header.tsx
@@ -3,7 +3,7 @@ import Logo from '../../assets/logo_white.svg'
 
 const Header = () => {
   return (
-    <div className="flex w-full items-center justify-center p-2 bg-purple-900">
+    <div className="flex w-full items-center justify-center p-2 bg-purple-900 flex-col max-w-6xl  px-[5%] md:px-[10%]">
       <img src={Logo} className="h-8" alt="logo" />
     </div>
   )
