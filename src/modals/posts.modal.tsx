30d6a20b82bcc31314eca692f7d4217ba3af062a
angelicawillrich <angelica@willri.ch>
2023-05-10 08:52:48 +0200

    post modal

--- src/modals/posts.modal.tsx
+++ src/modals/posts.modal.tsx
@@ -1,77 +1,104 @@
 /* eslint-disable max-len */
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { IoMdCloseCircle } from 'react-icons/io'
 import Button from '../components/baseComponents/button'
 import Input from '../components/baseComponents/input'
 import Modal from '../components/baseComponents/modal'
-
-interface Posts {
-  id: number
-  imageUrl: string
-  text: string
-}
+import { createPost, deletePost } from '../api/user.api'
+import { StoreContext } from '../context/context'
+import { convertBase64 } from '../utils'
+import { path } from '../shared'
 
 interface PostsModalProps {
   onClose: () => void
-  // posts: Posts[]
 }
 
 interface FormState {
-  photo?: any
-  text?: string
+  filename?: any
+  description?: string
 }
 
-const posts = [
-  { id: 0, imageUrl: 'src/assets/dog1.png', text: 'Oi! Esta é a Mione!' },
-  { id: 1, imageUrl: 'src/assets/dog2.png', text: 'Essa é a Cacau!' },
-  { id: 2, imageUrl: 'src/assets/dog3.png', text: 'Bom dia!' },
-  { id: 3, imageUrl: 'src/assets/dog4.png', text: 'Essa é a Cacau!' },
-  { id: 4, imageUrl: 'src/assets/dog1.png', text: 'Oie!' },
-  { id: 5, imageUrl: 'src/assets/dog2.png', text: 'Essa é a Cacau!' },
-  { id: 6, imageUrl: 'src/assets/dog3.png', text: 'Oi! Esta é a Mione!' },
-  { id: 7, imageUrl: 'src/assets/dog4.png', text: 'Essa é a Cacau!' },
-]
+const initialState = {
+  filename: '',
+  description: '',
+}
 
-const PostsModal = ({ onClose /* , posts */ }: PostsModalProps) => {
-  const [formState, setFormState] = useState<FormState>()
+const PostsModal = ({ onClose }: PostsModalProps) => {
+  const [formState, setFormState] = useState<FormState>(initialState)
+  const [selectedPostImage, setSelectedPostImage] = useState<File | undefined>(undefined)
 
-  const onSavePost = () => {
-    console.log('save post')
-    if (formState) {
-      console.log('formState', formState)
+  const { getLoggedInPetSitter, loggedInPetSitter } = useContext(StoreContext)
+
+  const onSavePost = async () => {
+    try {
+      if (!loggedInPetSitter || !selectedPostImage || !formState.description) {
+        alert('Erro ao salvar post! Todos os campos são obrigatórios.')
+        return
+      }
+
+      const image = await convertBase64(selectedPostImage)
+      const addData = {
+        petSitterId: loggedInPetSitter._id,
+        filename: image,
+        description: formState.description,
+      }
+      await createPost(addData)
+      setFormState(initialState)
+      setSelectedPostImage(undefined)
+      getLoggedInPetSitter(loggedInPetSitter?._id)
+      alert('Post salvo com sucesso!')
+      onClose()
+    } catch (error: any) {
+      console.error({ error })
+      alert(JSON.parse(error.request.responseText).message)
     }
   }
 
-  const onDeletePost = (id: number) => {
-    console.log('delete post', id)
+  const onDeletePost = async (postId: string) => {
+    try {
+      if (!loggedInPetSitter?.posts) return
+
+      const updatedPosts = loggedInPetSitter?.posts.filter((post) => post._id !== postId)
+      const posts = updatedPosts.map((post) => {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+        const { _id, ...rest } = post
+        return { ...rest }
+      })
+
+      const deleteData = {
+        petSitterId: loggedInPetSitter?._id,
+        posts,
+      }
+
+      await deletePost(deleteData)
+      if (loggedInPetSitter) getLoggedInPetSitter(loggedInPetSitter?._id)
+      alert('Post removido com sucesso!')
+    } catch (error: any) {
+      console.error({ error })
+      alert(JSON.parse(error.request.responseText).message)
+    }
   }
 
   const handleImageSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedPost = e.target.files?.[0]
-    if (selectedPost) {
-      console.log('selectedPost', selectedPost)
-      setFormState((previousState) => ({ ...previousState, photo: selectedPost }))
+    const postImage = e.target.files?.[0]
+    if (postImage) {
+      setSelectedPostImage(postImage)
+      setFormState((previousState) => ({ ...previousState, filename: postImage }))
     }
   }
-  console.log('formState', formState)
 
   return (
     <Modal title="Postagens" onClose={onClose}>
-      <div className=" max-h-80 overflow-auto grid grid-cols-4 gap-2 grid-cols">
-        {posts.map((post) => (
-          <div key={post.id} className="relative">
-            <div>
-              <IoMdCloseCircle
-                className="absolute top-0 right-0 cursor-pointer"
-                onClick={() => onDeletePost(post.id)}
-              />
-              <img
-                src={post.imageUrl}
-                alt=""
-                className="w-28"
-              />
-            </div>
-            <span className="text-gray-400 text-xs font-medium">{post.text}</span>
+      <div className=" max-h-80 overflow-auto grid grid-cols-2 gap-2 grid-cols">
+        {loggedInPetSitter?.posts.map((post) => (
+          <div key={post._id} className="relative p-2 flex flex-col">
+            <IoMdCloseCircle
+              className="absolute top-0 right-0 cursor-pointer"
+              onClick={() => onDeletePost(post._id)}
+            />
+            <img src={`${path}${post.filename}`} alt="" />
+            <span className="text-gray-400 text-xs font-medium">{post.description}</span>
+            <span className="text-gray-400 text-[8px]">{new Date(post.date).toLocaleDateString('pt-BR')}</span>
           </div>
         ))}
       </div>
@@ -84,15 +111,18 @@ const PostsModal = ({ onClose /* , posts */ }: PostsModalProps) => {
           accept="image/*"
           onChange={(e) => handleImageSelect(e)}
           placeholder="Adicione mais fotos..."
+          required
         />
         <Input
           label=""
           type="text"
-          onChange={(e) => setFormState((previousState) => ({ ...previousState, text: e.target.value }))}
+          onChange={(e) => setFormState((previousState) => ({ ...previousState, description: e.target.value }))}
           placeholder="Adicione uma legenda..."
+          required
         />
         <Button onClick={() => onSavePost()}>Salvar nova postagem</Button>
       </div>
+
     </Modal>
   )
 }
