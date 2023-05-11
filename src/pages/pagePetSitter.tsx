0ad8c4d81b8f73dc7925a134d4e595768fdf10c5
angelicawillrich <angelica@willri.ch>
2023-05-11 08:29:36 +0200

    refactored page petSitter

--- src/pages/pagePetSitter.tsx
+++ src/pages/pagePetSitter.tsx
@@ -1,14 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsWhatsapp } from 'react-icons/bs'
-
-import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 import { useNavigate, useParams } from 'react-router-dom'
-import { Tooltip } from 'react-tooltip'
-import Button from '../components/baseComponents/button'
 import {
-  calculateRatingAverage, generateInitialsAvatar, getTileContent, showStars, tileClassName,
+  calculateRatingAverage, generateInitialsAvatar, showStars,
 } from '../utils'
 import SetAppointmentModal from '../modals/setAppointment.modal'
 import { StoreContext } from '../context/context'
@@ -17,11 +13,13 @@ import { IUser } from '../interfaces/interfaces'
 import { path, services, species } from '../shared'
 
 import 'react-tooltip/dist/react-tooltip.css'
+import Button from '../components/baseComponents/button'
+import PetSitterCalendar from '../components/petSitterCalendar'
 
 const PagePetSitter = () => {
   const [isSetAppointmentModalOpen, setIsSetAppointmentModalOpen] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
-  const [petSitterInfo, setPetSitterInfo] = useState<IUser>()
+  const [petSitter, setPetSitter] = useState<IUser | undefined>()
 
   const navigate = useNavigate()
 
@@ -33,7 +31,7 @@ const PagePetSitter = () => {
       setIsLoading(true)
       if (petSitterId) {
         const result = await (await getPetSitterById(petSitterId))
-        setPetSitterInfo(result)
+        setPetSitter(result)
       } else {
         alert('PetSitter não encontrado.')
         navigate('/')
@@ -61,52 +59,52 @@ const PagePetSitter = () => {
       ? (<span>CARREGANDO...</span>)
       : (
         <div className="flex flex-col flex-3 w-full h-full gap-5 md:gap-10 justify-center md:flex-row">
-          {petSitterInfo?._id && loggedInUser?._id && isSetAppointmentModalOpen
-            && <SetAppointmentModal petSitterId={petSitterInfo._id} userId={loggedInUser._id} onClose={() => setIsSetAppointmentModalOpen(false)} />}
+          {petSitter?._id && loggedInUser?._id && isSetAppointmentModalOpen
+            && <SetAppointmentModal petSitterId={petSitter._id} userId={loggedInUser._id} onClose={() => setIsSetAppointmentModalOpen(false)} />}
           <div className="flex flex-col flex-1 h-full basis-3/5 divide-y divide-y-reverse divide-gray-100">
             <div>
               <div className="flex flex-row gap-4">
                 <div className="flex justify-center items-center mb-3">
-                  {petSitterInfo?.profilePicture
+                  {petSitter?.profilePicture
                     ? (
                       <img
-                        src={`${path}${petSitterInfo?.profilePicture}`}
+                        src={`${path}${petSitter?.profilePicture}`}
                         alt="dummy1"
                         className="w-16 h-16 rounded-full"
                         onError={onImageError}
                       />
                     )
-                    : (generateInitialsAvatar(petSitterInfo?.name || ''))}
+                    : (generateInitialsAvatar(petSitter?.name || ''))}
 
                   <div className="fley flex-col ml-2">
-                    <h1>{petSitterInfo?.name}</h1>
+                    <h1>{petSitter?.name}</h1>
                   </div>
                 </div>
               </div>
               <div className="flex flex-col text-gray-900 text-base mb-3">
-                <span>{petSitterInfo?.address}</span>
+                <span>{petSitter?.address}</span>
                 <span>
-                  {petSitterInfo?.district}
+                  {petSitter?.district}
                   {' '}
                   -
                   {' '}
-                  {petSitterInfo?.cityName}
+                  {petSitter?.cityName}
                   {' '}
                   -
                   {' '}
-                  {petSitterInfo?.stateName}
+                  {petSitter?.stateName}
                 </span>
                 <div className="flex flex-row items-center gap-2">
                   {' '}
                   <HiOutlineMail className="w-4 h-4" />
                   <span>
-                    {petSitterInfo?.email}
+                    {petSitter?.email}
                   </span>
                 </div>
                 <div className="flex flex-row items-center gap-2">
                   <BsWhatsapp className="w-4 h-4" />
                   <span>
-                    {petSitterInfo?.phone}
+                    {petSitter?.phone}
                   </span>
                 </div>
 
@@ -116,13 +114,13 @@ const PagePetSitter = () => {
             <div className="mt-4 pb-3">
               <h1>Conheça este(a) PetSitter</h1>
               <span>
-                {petSitterInfo?.petSitterInfo?.others || ''}
+                {petSitter?.petSitterInfo?.others || ''}
               </span>
             </div>
             <div className="mt-4 pb-3">
               <h1>Serviços e preços</h1>
-              {petSitterInfo?.petSitterInfo.services.length
-                ? petSitterInfo.petSitterInfo.services.map((petSitterService) => (
+              {petSitter?.petSitterInfo.services.length
+                ? petSitter.petSitterInfo.services.map((petSitterService) => (
                   <div key={petSitterService.serviceId} className="flex justify-between items-center">
                     <span>{services.find((service) => service.id === petSitterService.serviceId)?.label }</span>
                     <span>
@@ -141,7 +139,7 @@ const PagePetSitter = () => {
                   <specie.icon
                     key={specie.id}
                     className={`w-12 h-12 m-4 transition-all
-                      ${petSitterInfo?.petSitterInfo.allowedPets.includes(specie.id)
+                      ${petSitter?.petSitterInfo.allowedPets.includes(specie.id)
                       ? 'text-purple-900 hover:scale-110'
                       : 'text-gray-100'}`}
                   />
@@ -154,7 +152,7 @@ const PagePetSitter = () => {
                 <h1 className="mb-3">Posts</h1>
               </div>
               <div className="max-h-96 overflow-auto grid grid-cols-4 gap-2 grid-cols">
-                {petSitterInfo?.posts.map((post) => (
+                {petSitter?.posts.map((post) => (
                   <div key={post._id} className="flex flex-col">
                     <img
                       src={`${path}${post?.filename}`}
@@ -172,49 +170,22 @@ const PagePetSitter = () => {
           <div className="flex flex-col flex-1 h-full basis-2/5 divide-y divide-y-reverse divide-gray-100">
             <h1 className="mb-3">Agenda</h1>
             <div className="w-full">
-
-              <div className="w-fit">
-                <Calendar
-                  tileClassName={(date) => tileClassName({
-                    date: date.date,
-                    availableDates: petSitterInfo?.availableDates.map((availableDate) => {
-                      return {
-                        ...availableDate, weekDays: availableDate.weekDaysAndTime.map((day) => day.weekday),
-                      }
-                    }),
-                    bookings: petSitterInfo?.bookings,
-                  })}
-                  tileContent={(date) => getTileContent({
-                    date: date.date,
-                    availableDates: petSitterInfo?.availableDates.map((availableDate) => {
-                      return {
-                        ...availableDate, weekDays: availableDate.weekDaysAndTime.map((day) => day.weekday),
-                      }
-                    }),
-                  })}
-                  selectRange
-                />
-                <Tooltip id="my-tooltip" />
-                <div className="flex gap-2 mb-4">
-                  <span className="text-red-900 font-bold">*Ocupado</span>
-                  <span className="text-[#637644] font-bold">*Disponível</span>
-                </div>
-                <Button
-                  className="mb-3"
-                  onClick={() => setIsSetAppointmentModalOpen(true)}
-                >
-                  Solicitar agendamento
-                </Button>
-              </div>
+              <PetSitterCalendar petSitter={petSitter} />
+              <Button
+                className="mb-3"
+                onClick={() => setIsSetAppointmentModalOpen(true)}
+              >
+                Solicitar agendamento
+              </Button>
             </div>
             <div className="mt-4">
               <h1 className="mb-3">
                 Avaliações
                 {' '}
-                {petSitterInfo?.ratingsReceived && petSitterInfo?.ratingsReceived?.length > 0 && `${calculateRatingAverage(petSitterInfo.ratingsReceived)}/5`}
+                {petSitter?.ratingsReceived && petSitter?.ratingsReceived?.length > 0 && `${calculateRatingAverage(petSitter.ratingsReceived)}/5`}
               </h1>
-              {petSitterInfo?.ratingsReceived.length
-                ? petSitterInfo?.ratingsReceived.map((rating) => (
+              {petSitter?.ratingsReceived.length
+                ? petSitter?.ratingsReceived.map((rating) => (
                   <div key={rating._id} className="flex flex-col mb-4">
                     <div className="flex flex-row">
                       {showStars(rating.rating)}
