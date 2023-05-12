c557ac28228d4bfdaa4855d45ddcf19869355477
angelicawillrich <angelica@willri.ch>
2023-05-12 15:55:03 +0200

    added dates verification to set appointment modal

--- src/modals/setAppointment.modal.tsx
+++ src/modals/setAppointment.modal.tsx
@@ -45,13 +45,23 @@ const SetAppointmentModal = ({ petSitterId, userId, onClose }: SetAppointmentMod
   }, [])
 
   const handleCreateAppointment = async () => {
+    if (!formState.service || !formState.initialDate || !formState.finalDate || !formState.initialTime || !formState.finalTime) {
+      alert('Todos os campos sao obrigatórios.')
+      return
+    }
+
+    if (formState.finalDate < formState.initialDate) {
+      alert('Data final deve ser maior do que a data inicial.')
+      return
+    }
+
     try {
       await createBooking(formState)
       alert('A sua solicitação foi enviada ao PetSitter. Aguarde pela resposta que será enviada por e-mail.')
       await getLoggedInUser(formState.userId)
-    } catch (error) {
+    } catch (error: any) {
       console.error(error)
-      alert('Não foi possível solicitar o agendamento. Tente novamente.')
+      alert(JSON.parse(error.request.responseText).message)
     }
   }
   const onChangeForm = (field: keyof IFormState, value: string) => {
@@ -60,60 +70,64 @@ const SetAppointmentModal = ({ petSitterId, userId, onClose }: SetAppointmentMod
 
   return (
     <Modal title="Agendamento" onClose={onClose}>
-      <form onSubmit={handleCreateAppointment}>
+      <div className="flex flex-col w-full p-4 justify-center items-center gap-4">
         <div className="flex flex-col w-full p-4 justify-center items-center gap-4">
-          <div className="flex flex-col w-full p-4 justify-center items-center gap-4">
-            <Dropdown
-              id="service"
-              label="Selecione o servico"
-              list={services}
-              value={formState.service}
-              onChange={(e) => onChangeForm('service', e.target.value)}
+          <Dropdown
+            id="service"
+            label="Selecione o servico"
+            list={services}
+            value={formState.service}
+            onChange={(e) => onChangeForm('service', e.target.value)}
+            required
+          />
+          <div className="flex flex-row w-full gap-4">
+            <Input
+              type="date"
+              label="Data início*"
+              value={formState.initialDate}
+              onChange={(e) => onChangeForm('initialDate', e.target.value)}
+              min={moment(new Date()).format('YYYY-MM-DD')}
               required
             />
-            <div className="flex flex-row w-full gap-4">
-              <Input
-                type="date"
-                label="Data início*"
-                value={formState.initialDate}
-                onChange={(e) => onChangeForm('initialDate', e.target.value)}
-                min={moment(new Date()).format('YYYY-MM-DD')}
-                required
-              />
-              <Input
-                type="time"
-                label="Hora início*"
-                value={formState.initialTime}
-                onChange={(e) => onChangeForm('initialTime', e.target.value)}
-                required
-              />
-            </div>
-            <div className="flex flex-row w-full gap-4">
-              <Input
-                type="date"
-                label="Data fim*"
-                value={formState.finalDate}
-                onChange={(e) => onChangeForm('finalDate', e.target.value)}
-                min={moment(new Date(formState.initialDate)).format('YYYY-MM-DD')}
-                required
-              />
-              <Input
-                type="time"
-                label="Hora fim*"
-                value={formState.finalTime}
-                onChange={(e) => onChangeForm('finalTime', e.target.value)}
-                required
-              />
-            </div>
+            <Input
+              type="time"
+              label="Hora início*"
+              value={formState.initialTime}
+              onChange={(e) => onChangeForm('initialTime', e.target.value)}
+              required
+            />
           </div>
-          <Button type="submit">Solicitar agendamento</Button>
-          <span>
-            <b>Atenção:</b>
-            {' '}
-            O agendamento precisa ser aprovado pelo PetSitter. Um e-mail será enviado com a resposta do PetSitter.
-          </span>
+          <div className="flex flex-row w-full gap-4">
+            <Input
+              type="date"
+              label="Data fim*"
+              value={formState.finalDate}
+              onChange={(e) => onChangeForm('finalDate', e.target.value)}
+              min={moment(new Date(formState.initialDate)).format('YYYY-MM-DD')}
+              required
+            />
+            <Input
+              type="time"
+              label="Hora fim*"
+              value={formState.finalTime}
+              onChange={(e) => onChangeForm('finalTime', e.target.value)}
+              required
+            />
+          </div>
         </div>
-      </form>
+        <Button
+          type="button"
+          onClick={() => handleCreateAppointment()}
+        >
+          Solicitar agendamento
+
+        </Button>
+        <span>
+          <b>Atenção:</b>
+          {' '}
+          O agendamento precisa ser aprovado pelo PetSitter. Um e-mail será enviado com a resposta do PetSitter.
+        </span>
+      </div>
     </Modal>
   )
 }
