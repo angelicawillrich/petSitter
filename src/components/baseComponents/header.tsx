9f023ec9b5367b2b161c4278f3c4e55bbdc6cc33
angelicawillrich <angelica@willri.ch>
2023-05-13 18:29:58 +0200

    menu

--- src/components/baseComponents/header.tsx
+++ src/components/baseComponents/header.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Logo from '../../assets/logo_white.svg'
+import Menu from '../menu'
 
 const Header = () => {
   return (
-    <div className="flex w-full items-center justify-center p-2 bg-purple-900 flex-col max-w-6xl  px-[5%] md:px-[10%]">
+    <div className="flex w-full items-center justify-center p-2 bg-purple-900 flex-col max-w-6xl  px-[5%] md:px-[10%] relative">
       <img src={Logo} className="h-8" alt="logo" />
+      <Menu />
     </div>
   )
 }
---
+++ src/components/menu.tsx
@@ -0,0 +1,84 @@
+import React, { useContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { MdLogout } from 'react-icons/md'
+import { StoreContext } from '../context/context'
+import { generateInitialsAvatar } from '../utils'
+import { path } from '../shared'
+import { logout } from '../api/user.api'
+
+const Menu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const navigate = useNavigate()
+
+  const { loggedInUser } = useContext(StoreContext)
+
+  const handleLogout = async () => {
+    await logout()
+    setIsMenuOpen(false)
+    navigate('/login')
+  }
+  return (
+    loggedInUser && loggedInUser.name
+      ? (
+        <div className="flex flex-col max-w-xs absolute right-0 top-1 transition-all">
+          <button
+            type="button"
+            className="right-1 absolute w-10 h-10"
+            onClick={() => setIsMenuOpen((previousState) => !previousState)}
+          >
+            {loggedInUser?.profilePicture
+              ? (
+                <img
+                  src={`${path}${loggedInUser.profilePicture}`}
+                  alt="Foto de perfil"
+                  className="w-9 h-9 rounded-full"
+                />
+              )
+              : generateInitialsAvatar(loggedInUser?.name || '')}
+          </button>
+          {isMenuOpen
+      && (
+      <div className="flex flex-col mt-12 shadow-md p-2 bg-white gap-3">
+        <button
+          type="button"
+          className="hover:text-gray-900 transition-all w-fit"
+          onClick={() => {
+            navigate('/profile')
+            setIsMenuOpen(false)
+          }}
+        >
+          Editar dados pessoais
+
+        </button>
+        <button
+          type="button"
+          className="hover:text-gray-900 transition-all w-fit"
+          onClick={() => {
+            navigate('/pets')
+            setIsMenuOpen(false)
+          }}
+        >
+          Editar pets
+
+        </button>
+        <div className="h-0.5 w-full bg-purple-100" />
+        <button
+          type="button"
+          className="flex flex-row items-center gap-2 hover:text-gray-900 transition-all"
+          onClick={() => handleLogout()}
+        >
+          <MdLogout />
+          Sair
+        </button>
+      </div>
+      )}
+
+        </div>
+      )
+      : <></>
+
+  )
+}
+
+export default Menu
