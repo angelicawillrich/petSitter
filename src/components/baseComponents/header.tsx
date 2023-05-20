6e9e422777b4e99313eebfc5dba16541632f6ebc
angelicawillrich <angelica@willri.ch>
2023-05-20 17:25:22 +0200

    added link to home to the header's logo

--- src/components/baseComponents/header.tsx
+++ src/components/baseComponents/header.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Logo from '../../assets/logo_white.svg'
 import Menu from '../menu'
+import { StoreContext } from '../../context/context'
 
 const Header = () => {
+  const { loggedInUser } = useContext(StoreContext)
+  const navigate = useNavigate()
+
+  const redirect = () => {
+    if (loggedInUser?.isPetSitter) {
+      navigate(`/homepetsitter/${loggedInUser?._id}`)
+    } else {
+      navigate('/')
+    }
+  }
   return (
     <div className="flex w-full items-center justify-center p-2 bg-purple-900 flex-col max-w-6xl  px-[5%] md:px-[10%] relative">
-      <img src={Logo} className="h-8" alt="logo" />
+      <button type="button" onClick={() => redirect()}>
+        <img src={Logo} className="h-8" alt="logo" />
+      </button>
       <Menu />
     </div>
   )
